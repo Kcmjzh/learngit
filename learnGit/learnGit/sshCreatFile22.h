cad3b25b829795eed254af7f613995f55421a386
zhenqirong <364827317@qq.com>
2017-03-18 00:07:52 +0800

    添加sshcreatfile文件

---
+++ learnGit/learnGit/sshCreatFile22.h
@@ -0,0 +1,13 @@
+//
+//  sshCreatFile22.h
+//  learnGit
+//
+//  Created by aa on 2017/3/18.
+//  Copyright © 2017年 itZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sshCreatFile22 : UIView
+
+@end
---
+++ learnGit/learnGit/sshCreatFile22.m
@@ -0,0 +1,21 @@
+//
+//  sshCreatFile22.m
+//  learnGit
+//
+//  Created by aa on 2017/3/18.
+//  Copyright © 2017年 itZhen. All rights reserved.
+//
+
+#import "sshCreatFile22.h"
+
+@implementation sshCreatFile22
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
