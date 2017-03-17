d5c35db54f70f5537d357b81e249d17287bb6229
zhenqirong <364827317@qq.com>
2017-03-18 00:06:46 +0800

    添加httpcreatfile文件

---
+++ learnGit/learnGit/httpCreatFile.h
@@ -0,0 +1,13 @@
+//
+//  httpCreatFile.h
+//  learnGit
+//
+//  Created by aa on 2017/3/18.
+//  Copyright © 2017年 itZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface httpCreatFile : UIView
+
+@end
---
+++ learnGit/learnGit/httpCreatFile.m
@@ -0,0 +1,21 @@
+//
+//  httpCreatFile.m
+//  learnGit
+//
+//  Created by aa on 2017/3/18.
+//  Copyright © 2017年 itZhen. All rights reserved.
+//
+
+#import "httpCreatFile.h"
+
+@implementation httpCreatFile
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
