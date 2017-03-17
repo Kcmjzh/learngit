b6a4ba28e65d8f9db7c05945fd62665313a90a6e
zhenqirong <364827317@qq.com>
2017-03-18 01:33:39 +0800

    添加test2文件

---
+++ learnGit/learnGit/test2.h
@@ -0,0 +1,13 @@
+//
+//  test2.h
+//  learnGit
+//
+//  Created by aa on 2017/3/18.
+//  Copyright © 2017年 itZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test2 : UIView
+
+@end
---
+++ learnGit/learnGit/test2.m
@@ -0,0 +1,21 @@
+//
+//  test2.m
+//  learnGit
+//
+//  Created by aa on 2017/3/18.
+//  Copyright © 2017年 itZhen. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
